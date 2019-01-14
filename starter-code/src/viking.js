6db58c3695afb80deb389333b4698dfc221e1bae
Rojiblanco182 <robertosalinas1988@gmail.com>
2019-01-14 21:42:14 +0100

    updated version

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -4,71 +4,62 @@ function Soldier(healthArg, strengthArg) {
   this.strength = strengthArg;
 }
 
-Soldier.prototype.attack = function(){
+Soldier.prototype.attack = function () {
   return this.strength;
 }
 
-Soldier.prototype.receiveDamage = function(damage){
+Soldier.prototype.receiveDamage = function (damage) {
   this.health -= damage;
 }
 
 // Viking
 function Viking(nameArg, healthArg, strengthArg) {
   this.name = nameArg;
-  Soldier.call (this, healthArg, strengthArg);
-  this.receiveDamage = function(damage){
-    this.health -= damage;
-    if (this.health > 0) {
-      return this.name + " has received " + damage + " points of damage";
-    }
-    return this.name + " has died in act of combat";
-  }
-  this.battleCry = function () {
-    return "Odin Owns You All!";
-  }
+  Soldier.call(this, healthArg, strengthArg);
 }
 
 Viking.prototype = Object.create(Soldier.prototype);
 Viking.prototype.constructor = Viking;
 
 
-// Viking.prototype.receiveDamage = function(damage){
-//   this.health -= damage;
-//   if (this.health > 0) {
-//     return this.name + " has received " + damage + "points of damage";
-//   }
-//   return this.name + " has died in act of combat";
-// }
+Viking.prototype.receiveDamage = function (damage) {
+  this.health -= damage;
+  if (this.health > 0) {
+    return this.name + " has received " + damage + " points of damage";
+  }
+  return this.name + " has died in act of combat";
+}
 
-// Por qué no funciona si lo declaro fuera de la función Viking?
-// Traté de hacerlo así, con prototype, pero no me funciona(battleCry tampoco si la creo fuera)
-// al contrario que en las que declaré en Soldier, las cuales sí funcionan desde fuera.
+Viking.prototype.battleCry = function () {
+  return "Odin Owns You All!";
+}
 
 // Saxon
 function Saxon(healthArg, strengthArg) {
   Soldier.call(this, healthArg, strengthArg);
-  this.receiveDamage = function(damage) {
-    this.health -= damage;
-    if (this.health > 0) {
-      return "A Saxon has received " + damage + " points of damage";
-    }
-    return "A Saxon has died in combat";
-  }
 }
 
 Saxon.prototype = Object.create(Soldier.prototype);
 Saxon.prototype.constructor = Saxon;
+
+Saxon.prototype.receiveDamage = function (damage) {
+  this.health -= damage;
+  if (this.health > 0) {
+    return "A Saxon has received " + damage + " points of damage";
+  }
+  return "A Saxon has died in combat";
+}
 
 // War
 function War() {
   this.vikingArmy = [];
   this.saxonArmy = [];
 
-  this.addViking = function(viking) {
+  this.addViking = function (viking) {
     this.vikingArmy.push(viking);
   }
 
-  this.addSaxon = function(saxon) {
+  this.addSaxon = function (saxon) {
     this.saxonArmy.push(saxon);
   }
 
@@ -77,31 +68,31 @@ function War() {
   var vikingStrength = 0;
   var saxonStrength = 0;
 
-  this.vikingAttack = function() {
+  this.vikingAttack = function () {
     randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
     randomViking = Math.floor(Math.random() * this.vikingArmy.length);
     vikingStrength = this.vikingArmy[randomViking].strength;
 
-    var result = this.saxonArmy[randomSaxon].receiveDamage (vikingStrength);
+    var result = this.saxonArmy[randomSaxon].receiveDamage(vikingStrength);
     if (this.saxonArmy[randomSaxon].health <= 0) {
       this.saxonArmy.splice(randomSaxon, 1);
     }
     return result;
   }
 
-  this.saxonAttack = function() {
+  this.saxonAttack = function () {
     randomSaxon = Math.floor(Math.random() * this.saxonArmy.length);
     randomViking = Math.floor(Math.random() * this.vikingArmy.length);
     saxonStrength = this.saxonArmy[randomSaxon].strength;
 
-    var result = this.vikingArmy[randomViking].receiveDamage (saxonStrength);
+    var result = this.vikingArmy[randomViking].receiveDamage(saxonStrength);
     if (this.vikingArmy[randomViking].health <= 0) {
       this.vikingArmy.splice(randomViking, 1);
     }
     return result;
   }
 
-  this.showStatus = function() {
+  this.showStatus = function () {
     var aliveSaxons = this.saxonArmy.length;
     var aliveVikings = this.vikingArmy.length;
 
